06f42f66e2e74cb6e73d05af6f89a9dcbf5b726a
Tyler Lee <tylerblee9@gmail.com>
2024-08-01 23:45:52 -0500

    Preparations Part 1
    After some testing with tic-tac-toe, I have a successful model running in stable-baselines3 that should be able to attempt to learn any environment using PPO.
    Importantly, this includes action masking, which was built off of David Foster's SIMPLE project. The legal actions in a state are sent as a boolean vector appended to the observation. The custom network takes this in and separates the two, using the legal actions as a mask of the logits output by the policy head.
    
    The other major change is the creation of a game randomizer. In order to improve training quality, I created a method that resets and then randomly sets up a game of ROOT. Hopefully this will give the models a more diverse set of games to deal with and allow them to develop more robust strategies.
    
    Other changes:
    - Allowed the user to set a max number of actions when creating the environment
    - Fixed a bug where the if in a battle, the attacker had a choice of what to damage when taking hits, they would end up destroying all of their cardboard in that clearing instead of the exact number of hits left to take (forgot a "- 1")
    - Fixed a bug where if the Vagabond was coalitioned with the Woodland Alliance, they were not allowed to do the 'strike' action on any Eyrie targets

---
+++ envs/root4pbasemarquise.py
@@ -0,0 +1,195 @@
+import gym
+import numpy as np
+import os
+import time
+
+import config
+
+
+from .envs.rootGameClasses.rootMechanics import *
+
+# Game Obs length: 1487 (down from 4474 i think)
+# docker-compose exec app tensorboard --logdir ./logs
+# notes:
+# - os (optimizer stepsize) I think is the 'learning rate' parameter
+#        - Should be decreased linearly over training to 0 or very small
+#        - Some papers have it as small as 1e-6 at the end
+
+# start
+# docker-compose exec app mpirun -np 2 python3 train.py -e root4pbasemarquise -ne 25 -ef 20480 -tpa 2048 -ent 0.025 -ob 128 -g 0.995 -oe 6 -t 0
+
+MARQUISE_ID = 0
+EYRIE_ID = 1
+ALLIANCE_ID = 2
+VAGABOND_ID = 3
+
+MAIN_PLAYER_ID = MARQUISE_ID
+BEST_MODEL_CHANCE = 0.8
+OPPONENT_INFO = [(EYRIE_ID,"root4pbaseeyrie"),(ALLIANCE_ID,"root4pbasealliance"),(VAGABOND_ID,"root4pbasevagabond")]
+
+# marquise - 1538 obs / 548 actions
+# eyrie - 1522 / 492
+# alliance - 1571 / 530
+# vagabond - 1547 / 604
+
+class rootEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self, verbose = False, manual = False):
+        super(rootEnv, self).__init__()
+        self.name = 'root4pbasemarquise'
+        self.n_players = 1
+        self.manual = manual
+
+        self.action_space = gym.spaces.Discrete(548)
+        self.observation_space = gym.spaces.Box(-1, 1, (
+            1538
+            + self.action_space.n
+            , )
+        )  
+        self.verbose = verbose
+        self.env = RootGame(CHOSEN_MAP,STANDARD_DECK_COMP)
+
+        self.opp_model_pool = {}
+        self.best_opponent_models = [None] * 4
+
+        for opp_id,opp_env_name in OPPONENT_INFO:
+            logger.info(f"\nLoading {ID_TO_PLAYER[opp_id]} Models to train against...")
+            self.opp_model_pool[opp_id] = []
+            opp_dir = os.path.join(config.MODELDIR, opp_env_name)
+
+            opp_files = os.listdir(opp_dir)
+            for model_file_name in opp_files:
+                full_path = os.path.join(opp_dir,model_file_name)
+                if os.path.exists(full_path):
+                    logger.info(f"\tLoading {model_file_name} to play / train against...")
+                    cont = True
+                    while cont:
+                        try:
+                            ppo_model = PPO1.load(full_path)
+                            cont = False
+                        except Exception as e:
+                            time.sleep(5)
+                            print(e)
+                else:
+                    raise Exception(f'\n{full_path} not found!')
+
+                this_model = utils.agents.Agent(ID_TO_PLAYER[opp_id] + " " + model_file_name[:-4], ppo_model)
+                if model_file_name == "best_model.zip":
+                    self.best_opponent_models[opp_id] = this_model
+                else:
+                    self.opp_model_pool[opp_id].append(this_model)
+
+    @property
+    def current_player(self) -> Player:
+        return self.players[self.current_player_num]
+    
+    @property
+    def opposing_player(self) -> Player:
+        i = (self.current_player_num + 1) % 2
+        return self.players[i]
+        
+    @property
+    def observation(self):
+        ret = np.append(self.env.get_marquise_observation(),self.legal_actions)
+        return ret
+
+    @property
+    def legal_actions(self):
+        ret = np.zeros(self.action_space.n)
+        ret.put(self.env.legal_actions(), 1)
+        return ret
+    
+    def run_opponent_turn(self):
+        opponent_id = self.env.to_play()
+        opp = self.opponent_models[opponent_id]
+        logger.debug(f'\n{opp.name} model choices')
+        # action_chosen = random.choice(self.get_legal_action_numbers())
+
+        if opponent_id == PIND_EYRIE:
+            la = np.zeros(492)
+            la.put(self.env.legal_actions(),1)
+            opponent_obs = np.append(self.env.get_eyrie_observation(), la)
+        elif opponent_id == PIND_ALLIANCE:
+            la = np.zeros(530)
+            la.put(self.env.legal_actions(),1)
+            opponent_obs = np.append(self.env.get_alliance_observation(), la)
+        else:
+            la = np.zeros(604)
+            la.put(self.env.legal_actions(),1)
+            opponent_obs = np.append(self.env.get_vagabond_observation(), la)
+
+        action_probs = opp.model.action_probability(opponent_obs)
+        value = opp.model.policy_pi.value(np.array([opponent_obs]))[0]
+        logger.debug(f"Value {value:.3f}")
+
+        opp.print_top_actions(action_probs)
+
+        action_probs = utils.agents.mask_actions(la, action_probs)
+        logger.debug('Masked ->')
+        opp.print_top_actions(action_probs)
+
+        action_chosen = utils.agents.sample_action(action_probs)
+        logger.debug(f'Sampled action {action_chosen} chosen')
+        
+        reward, done = self.env.step(action_chosen)
+
+        return reward,done
+
+    def step(self, action):
+        reward, self.done = self.env.step(action)
+        self.current_player_num = (self.env.to_play() - MAIN_PLAYER_ID)
+
+        # play the game until it comes back to the main player's turn or the game ends
+        while self.current_player_num != 0 and (not self.done):
+            r, self.done = self.run_opponent_turn()
+
+            reward = [reward[i] + r[i] for i in range(4)]
+            self.current_player_num = (self.env.to_play() - MAIN_PLAYER_ID)
+
+        return self.observation, reward[MAIN_PLAYER_ID:MAIN_PLAYER_ID + 1], self.done, {}
+
+    def reset(self):
+        self.done = False
+        self.env.reset()
+        self.current_player_num = (self.env.to_play() - MAIN_PLAYER_ID)
+
+        self.opponent_models = [None] * 4
+        for i,name in OPPONENT_INFO:
+            if random.random() < BEST_MODEL_CHANCE:
+                self.opponent_models[i] = self.best_opponent_models[i]
+            else:
+                self.opponent_models[i] = random.choice(self.opp_model_pool[i])
+            logger.debug(f"> Chosen {ID_TO_PLAYER[i]} Model: {self.opponent_models[i].name}")
+
+        # play the game until it comes back to the main player's turn or the game ends
+        while self.current_player_num != 0 and (not self.done):
+            r, self.done = self.run_opponent_turn()
+            
+            self.current_player_num = (self.env.to_play() - MAIN_PLAYER_ID)
+
+        return self.observation
+
+    def render(self, mode='human', close=False):
+        if close:
+            return
+        
+        # self.env.render()
+
+        if self.verbose:
+            logger.debug(f'\nObservation: \n{[i if o == 1 else (i,o) for i,o in enumerate(self.observation) if o != 0]}')
+        
+        if not self.done:
+            logger.debug(f'\nLegal actions: {[i for i,o in enumerate(self.legal_actions) if o != 0]}')
+
+
+    def rules_move(self):
+        raise Exception('Rules based agent is not yet implemented for Geschenkt!')
+
+# if __name__ == "__main__":
+#     env = rootEnv()
+#     env.reset()
+#     done = False
+#     total_rewards = np.zeros(1)
+#     while not done:
+#         legal_actions = env.legal_actions
---
+++ models/marquise_model.py
@@ -0,0 +1,251 @@
+# marquise 548
+# eyrie 492
+# alliance 530
+# vagabond 604
+
+from gymnasium import Space
+# import tensorflow as tf
+# tf.get_logger().setLevel('INFO')
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.distributions import CategoricalDistribution
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.type_aliases import PyTorchObs
+
+
+ACTIONS = 548
+OBS_LENGTH = 1538
+
+RESNET_FEATURE_SIZE = 256
+# final layer sizes before output layer
+POLICY_FEATURE_SIZE = 128
+VALUE_FEATURE_SIZE = 128
+
+RESNET_DEPTH = 2
+VALUE_DEPTH = 2
+POLICY_DEPTH = 2
+
+def split_input(obs, split):
+    return   obs[:,:-split], obs[:,-split:]
+
+
+class CustomDense(nn.Module):
+    def __init__(self, input_dim, num_filters, batch_norm=False, activation='relu') -> None:
+        super().__init__()
+        self.dense = nn.Linear(in_features=input_dim, out_features=num_filters)
+        self.batch_norm = nn.BatchNorm1d(num_filters, momentum=0.9) if batch_norm else None
+        self.activation = getattr(F, activation) if activation else None
+    
+    def forward(self, x):
+        x = self.dense(x)
+        if self.batch_norm:
+            x = self.batch_norm(x)
+        if self.activation:
+            x = self.activation(x)
+
+        return x
+
+
+class CustomResidual(nn.Module):
+    def __init__(self, input_dim, num_filters) -> None:
+        super().__init__()
+        # this is just a placeholder linear layer, replaced later for specific input size
+        self.dense1 = CustomDense(input_dim, num_filters)
+        self.dense2 = CustomDense(num_filters, num_filters, activation=None)
+        self.activation = F.relu
+    
+    def forward(self, x):
+        shortcut = x
+
+        x = self.dense1(x)
+        x = self.dense2(x)
+        x += shortcut
+        x = self.activation(x)
+
+        return x
+
+
+class CustomResnetExtractor(nn.Module):
+    def __init__(self, input_dim) -> None:
+        super().__init__()
+        self.seq = nn.Sequential(CustomDense(input_dim=input_dim, num_filters=RESNET_FEATURE_SIZE))
+        for _ in range(RESNET_DEPTH):
+            self.seq.append(CustomResidual(RESNET_FEATURE_SIZE, RESNET_FEATURE_SIZE))
+
+    def forward(self, x):
+        return self.seq(x)
+
+
+class CustomValueHead(nn.Module):
+    def __init__(self, input_dim) -> None:
+        super().__init__()
+        self.seq = nn.Sequential(CustomDense(input_dim=input_dim, num_filters=VALUE_FEATURE_SIZE*2))
+        
+        self.seq.append(CustomDense(input_dim=VALUE_FEATURE_SIZE*2, num_filters=VALUE_FEATURE_SIZE))
+        
+        self.seq.append(
+            CustomDense(input_dim=VALUE_FEATURE_SIZE, num_filters=1, activation='tanh')
+        )
+
+    def forward(self, x):
+        return self.seq(x)
+
+# def value_head(y):
+#     for _ in range(VALUE_DEPTH):
+#         y = dense(y, FEATURE_SIZE)
+#     vf = dense(y, 1, batch_norm = False, activation = 'tanh', name='vf')
+#     q = dense(y, ACTIONS, batch_norm = False, activation = 'tanh', name='q')
+#     return vf, q
+
+
+class CustomPolicyHead(nn.Module):
+    def __init__(self, input_dim) -> None:
+        super().__init__()
+        self.seq = nn.Sequential(CustomDense(input_dim=input_dim, num_filters=2*POLICY_FEATURE_SIZE))
+        
+        self.seq.append(CustomDense(input_dim=2*POLICY_FEATURE_SIZE, num_filters=POLICY_FEATURE_SIZE))
+        
+        self.seq.append(
+            CustomDense(input_dim=POLICY_FEATURE_SIZE, num_filters=ACTIONS, activation=None)
+        )
+
+    def forward(self, x, legal_actions):
+        mask = (1 - legal_actions) * -1e8
+
+        return (self.seq(x) + mask)
+    
+
+# def policy_head(y, legal_actions):
+
+#     for _ in range(POLICY_DEPTH):
+#         y = dense(y, FEATURE_SIZE)
+#     policy = dense(y, ACTIONS, batch_norm = False, activation = None, name='pi')
+    
+#     mask = Lambda(lambda x: (1 - x) * -1e8)(legal_actions)   
+    
+#     policy = Add()([policy, mask])
+#     return policy
+
+
+class FeatureMaskExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: Space, features_dim: int):
+        super().__init__(observation_space, features_dim)
+        self.extractor = CustomResnetExtractor(input_dim=OBS_LENGTH)
+    
+    def forward(self, x):
+        obs, legal_actions = split_input(x, ACTIONS)
+        features = self.extractor(obs)
+        return features, legal_actions
+
+
+class CustomMLPExtractor(nn.Module):
+    """
+    Custom network for policy and value function.
+    It receives as input the features extracted by the features extractor.
+
+    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    """
+
+    def __init__(
+        self,
+        last_layer_dim_pi: int = 8,
+        last_layer_dim_vf: int = 8,
+    ):
+        super().__init__()
+
+        # IMPORTANT:
+        # Save output dimensions, used to create the distributions
+        self.latent_dim_pi = last_layer_dim_pi
+        self.latent_dim_vf = last_layer_dim_vf
+
+        # Policy network
+        self.policy_net = CustomPolicyHead(input_dim=RESNET_FEATURE_SIZE)
+        # Value network
+        self.value_net = CustomValueHead(input_dim=RESNET_FEATURE_SIZE)
+
+    def forward(self, features):
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features, legal_actions):
+        return self.policy_net(features, legal_actions)
+
+    def forward_critic(self, features):
+        return self.value_net(features)
+
+
+class CustomPolicy(ActorCriticPolicy):
+    def __init__(self, observation_space, action_space, lr_schedule, *args, **kwargs):
+        super(CustomPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            *args,
+            features_extractor_class=FeatureMaskExtractor,
+            features_extractor_kwargs=dict(features_dim=RESNET_FEATURE_SIZE),
+            **kwargs,
+        )
+        # self.device = get_device()
+
+    def _build_mlp_extractor(self):
+        self.mlp_extractor = CustomMLPExtractor()
+
+    def forward(self, obs, deterministic=False):
+        features, legal_actions = self.features_extractor(obs)
+        policy_logits = self.mlp_extractor.forward_actor(features, legal_actions)
+        values = self.mlp_extractor.forward_critic(features)
+        dist = CategoricalDistribution(ACTIONS)
+        dist.proba_distribution(policy_logits)
+
+        if deterministic:
+            actions = torch.argmax(policy_logits, dim=1)
+        else:
+            actions = dist.sample()
+        
+        log_prob = dist.log_prob(actions)
+        return actions, values, log_prob
+    
+    def _predict(self, obs, deterministic=False):
+        features, legal_actions = self.features_extractor(obs)
+        policy_logits = self.mlp_extractor.forward_actor(features, legal_actions)
+        dist = CategoricalDistribution(ACTIONS)
+        dist.proba_distribution(policy_logits)
+
+        if deterministic:
+            actions = torch.argmax(policy_logits, dim=1)
+        else:
+            actions = dist.sample()
+        
+        return actions
+    
+    def evaluate_actions(self, obs, actions):
+        features, legal_actions = self.features_extractor(obs)
+        policy_logits = self.mlp_extractor.forward_actor(features, legal_actions)
+        values = self.mlp_extractor.forward_critic(features)
+        dist = CategoricalDistribution(ACTIONS)
+        dist.proba_distribution(policy_logits)
+
+        log_prob = dist.log_prob(actions)
+        entropy = dist.entropy()
+        return values, log_prob, entropy
+
+    def predict_values(self, obs: PyTorchObs) -> torch.Tensor:
+        """
+        Get the estimated values according to the current policy given the observations.
+
+        :param obs: Observation
+        :return: the estimated values.
+        """
+        features, legal_actions = self.features_extractor(obs)
+        latent_vf = self.mlp_extractor.forward_critic(features)
+        return latent_vf
